892128de5c2c0579d6712d5ce776cf902e7f6755
kevin puscan <kevinjpuscan@gmail.com>
2020-03-17 22:50:43 -0500

    cors modified

--- api/controllers/v1/DashboardController.js
+++ api/controllers/v1/DashboardController.js
@@ -1,7 +1,7 @@
 module.exports = {
     lastWatt: async function (req,res){
         try {
-            let data = await Vista.getDatastore().sendNativeQuery('select *,watts/360 as watt from tp_last_electric;');
+            let data = await Vista.getDatastore().sendNativeQuery('select *,watts/360 as wh from tp_last_electric;');
             res.json(data.rows[0]);
         } catch (error) {
             res.status(500);
@@ -13,12 +13,12 @@ module.exports = {
         try {
             let data = await Vista.getDatastore().sendNativeQuery(`
             with rows as (
-                select ordinality,round((coalesce(w/360,0))::numeric,4) as watt,to_char((tiem_register + ordinality * (interval '10 second'))-interval '10 second','HH24:MM:SS') as tiem_registro,id
+                select ordinality,round((coalesce(w/360,0))::numeric,4) as wh,to_char((tiem_register + ordinality * (interval '10 second'))-interval '10 second','HH24:mi:SS') as tiem_registro,id
                   from tp_data_electric, unnest(watts) WITH ORDINALITY w
-                where tiem_register>= date_trunc('hour',now())-interval '3 hours'
+                where tiem_register>= date_trunc('hour',now())-interval '12 hours'
                 order by 4,1 asc)
-                select array(select watt from rows) as watts ,array(select tiem_registro from rows) as times`);
-            res.json(data.rows);
+                select array(select wh from rows) as series ,array(select tiem_registro from rows) as category`);
+            res.json(data.rows[0]);
         } catch (error) {
             res.status(500);
             res.send('Error al obtener la data');
